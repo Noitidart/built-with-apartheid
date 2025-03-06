748bfcc751483ebe549641ee7907e602f6bffad0
Noitidart <noitidart@hey.com>
2025-03-06 06:54:37 -0800

    Comment out lint errors blocking deploy

--- built-with-is-not-real/pages/api/hello.ts
+++ built-with-is-not-real/pages/api/hello.ts
@@ -1,12 +1,14 @@
-import type { NextRequest } from 'next/server'
-import { getRequestContext } from '@cloudflare/next-on-pages'
+// import type { NextRequest } from "next/server";
+// import { getRequestContext } from '@cloudflare/next-on-pages'
 
 export const config = {
-  runtime: 'edge',
-}
+  runtime: "edge",
+};
 
-export default async function handler(req: NextRequest) {
-  let responseText = 'Hello World'
+export default async function handler() {
+  // req: NextRequest
+
+  const responseText = "Hello World";
 
   // In the edge runtime you can use Bindings that are available in your application
   // (for more details see:
@@ -20,5 +22,5 @@ export default async function handler(req: NextRequest) {
   // const suffix = await myKv.get('suffix')
   // responseText += suffix
 
-  return new Response(responseText)
+  return new Response(responseText);
 }
