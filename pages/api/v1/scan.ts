6589ce8da2649e2b290e36fd86f71acb673a3c23
Noitidart <noitidart@protonmail.com>
2025-03-07 09:24:49 -0800

    Mimic google crawler to bypass icoi.net captcha

--- pages/api/v1/scan.ts
+++ pages/api/v1/scan.ts
@@ -95,17 +95,12 @@ async function fetchHtml(url: string): Promise<string> {
   const response = await fetch(url, {
     headers: {
       "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
-      Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.9",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+      Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
       Referer: "https://www.google.com/",
-      "Cache-Control": "max-age=0",
-      "Sec-Ch-Ua":
-        '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-      "Sec-Ch-Ua-Mobile": "?0",
-      "Sec-Ch-Ua-Platform": '"macOS"',
-      "Upgrade-Insecure-Requests": "1",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
     },
   });
 
