ded88f9f266912d535feb0213625914422e00d82
Noitidart <noitidart@hey.com>
2025-03-06 19:47:14 -0800

    Try to prevent captcha on scan fetch to some sites

--- pages/api/v1/scan.ts
+++ pages/api/v1/scan.ts
@@ -92,7 +92,22 @@ export default async function handler(req: NextRequest) {
 }
 
 async function fetchHtml(url: string): Promise<string> {
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.9",
+      Referer: "https://www.google.com/",
+      "Cache-Control": "max-age=0",
+      "Sec-Ch-Ua":
+        '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+      "Sec-Ch-Ua-Mobile": "?0",
+      "Sec-Ch-Ua-Platform": '"macOS"',
+      "Upgrade-Insecure-Requests": "1",
+    },
+  });
 
   return await response.text();
 }
